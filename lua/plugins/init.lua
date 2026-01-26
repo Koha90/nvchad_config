8327e69642a71a51680f37965a1813d42ea30d26
Koha90 <koha90@git.com>
2026-01-26 20:52:10 +0500

    включено автоформатирование и автоимпорты для go

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,5 +52,55 @@ return {
       })
     end,
     event = "BufRead",
-  }
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "html", "css", "go", "rust", "cmake", "python",
+        "javascript", "typescript", "markdown"
+  		},
+  	},
+  },
+
+  { "mfussenegger/nvim-dap" },
+
+  {
+    "dreamsofcode-io/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+    end
+  },
+
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+      require("todo-comments").setup {
+        keywords = {
+          FIX = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+          TODO = { icon = " ", color = "info" },
+          HACK = { icon = " ", color = "warning" },
+          WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+          PERF = { icon = " ", color = "default", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+          NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        },
+      }
+    end,
+  },
 }
