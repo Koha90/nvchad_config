70d9e80fb27d1b65891952b7559ca1cb9e9d8202
Koha90 <koha90@git.com>
2026-02-25 01:28:06 +0500

    changed other theme, uncomment formant on save, added highlight for treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -61,6 +61,7 @@ return {
         "typescript",
         "markdown",
       },
+      highlight = { enable = true },
     },
   },
 
